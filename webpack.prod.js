ddd170b9f8f1fb2901fefa5a53e25030b37172ff
Rafael de Sousa <rafael.desousa@dna.co.nz>
2020-08-06 10:11:25 +1200

    Updating build

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,13 +4,18 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const common = require('./webpack.common.js');
+const pkg = require('./package.json');
+const libraryName = pkg.name;
 
 module.exports = merge(common, {
     entry: './src/rating/rating.js',
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'index.js',
-        publicPath: 'http://localhost:8080/dist/'
+        library: libraryName,
+        libraryTarget: 'umd',
+        publicPath: '/dist/',
+        umdNamedDefine: true
     },
     mode: 'production',
     devtool: 'source-map',
@@ -43,5 +48,29 @@ module.exports = merge(common, {
             filename: '[name].css',
             chunkFilename: '[id].css'
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+            // '@babel/runtime': path.resolve(__dirname, './node_modules/@babel/runtime'),
+            // '@material-ui/core': path.resolve(__dirname, './node_modules/@material-ui/core'),
+            // '@material-ui/lab': path.resolve(__dirname, './node_modules/@material-ui/lab'),
+        }
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        }
+    }
 });
